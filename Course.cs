ff5c1da765ee77b785decbef51ebc734029489b0
Joshua Wecker <gershwinwecker@gmail.com>
2012-04-06 03:34:51 -0700

    Main.cs contains an entry point into application. MainWindow.cs and MainWindow.Designer.cs provide layout for initial application screen. Course.cs and CourseList.cs are classes containing data used by majors and students.

---
+++ Course.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MajorEvaluationApplication
+{
+    class Course
+    {
+        private int CRN, courseNumber;
+        private double gpa;
+        private string courseName;
+        
+        /*Construtor sets each course's
+        /*members to default values, subject 
+        /*to change.*/
+        public Course(){
+            CRN = 0;
+            courseNumber = 0;
+            gpa = 0.0;
+            courseName = null;
+        }
+
+        public int getCRN()
+        {
+            return CRN;
+        }
+        
+        public void setCRN(int CRN)
+        {
+            this.CRN = CRN;
+        }
+
+        public double getGpa()
+        {
+            return gpa;
+        }
+
+        public void setGpa(double Gpa)
+        {
+            this.gpa = CRN;
+        }
+
+        public string getCourseName()
+        {
+            return courseName;
+        }
+
+        public void setCourseName(string courseName)
+        {
+            this.courseName = courseName;
+        }
+
+        public int getCourseNumber()
+        {
+            return courseNumber;
+        }
+
+        public void setCourseNumber(int courseNumber)
+        {
+            this.courseNumber = courseNumber;
+        }
+
+
+
+    }
+}
---
+++ CourseList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MajorEvaluationApplication
+{
+    class CourseList
+    {
+        private List<Course> allCourses;
+        private string name;
+
+
+        public CourseList()
+        {
+            allCourses = null;
+        }
+
+        public double averageGpa()
+        {
+            double totalGpa = 0.0;
+            double avgGpa = 0.0;
+            foreach (Course c in allCourses)
+            {
+                totalGpa = totalGpa + c.getGpa();
+            }
+            avgGpa = totalGpa / allCourses.Count();
+            avgGpa = Math.Round(avgGpa, 2);
+            return avgGpa;
+        }
+    }
+}
