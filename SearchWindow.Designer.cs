940d19a88f7b8ce1644b79b016b562a977a66a2c
Joshua Wecker <gershwinwecker@gmail.com>
2012-04-15 13:41:56 -0700

    Added SearchWindow files which manage the student selection process of the application. Also updated several of our other files to interact with the SearchWindow

---
+++ SearchWindow.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MajorEvaluator
+{
+    partial class SearchWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 65);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Find Students";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(274, 203);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Browse Students";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(3, 16);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(268, 184);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "W#";
+            this.columnHeader1.Width = 72;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 91;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Name";
+            this.columnHeader3.Width = 102;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 32);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(125, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SearchWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 336);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SearchWindow";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.SearchWindow_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
---
+++ SearchWindow.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MajorEvaluator
+{
+    public partial class SearchWindow : Form
+    {
+
+        private String ID;
+        private String lastName;
+        private String firstName;
+
+        public SearchWindow()
+        {
+            InitializeComponent();
+            ID = null;
+            firstName = null;
+            lastName = null;
+        }
+
+        public String getId()
+        {
+            return ID;
+        }
+
+        public String getName()
+        {
+            return (this.lastName + ", " + this.firstName);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string SearchTerm = textBox1.Text;
+            string MyConString = "SERVER=tcekle.com;" +
+                "DATABASE=dummydb;" +
+                "UID=remote;" +
+                "PASSWORD=f3erePub;";
+            MySqlConnection connection = new MySqlConnection(MyConString);
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader Reader;
+            command.CommandText = "select ID, FIRST_NAME, LAST_NAME from G_PERSON where ID like '%" + SearchTerm + "%'";
+            connection.Open();
+            Reader = command.ExecuteReader();
+            while (Reader.Read())
+            {
+                String id = Reader["ID"].ToString();
+                String fname = Reader["FIRST_NAME"].ToString();
+                String lname = Reader["LAST_NAME"].ToString();
+                ListViewItem item = new ListViewItem(new[] { id, fname, lname });
+                listView1.Items.Add(item);
+                //string[] row1 = { "s1", "s2", "s3" };
+                //listView1.Items.Add("Column1Text").SubItems.AddRange(row1);
+                //ListViewItem lv = new ListViewItem(fname);
+                //lv.SubItems.Add(fname);
+                //lv.SubItems.Add(lname);
+                //listView1.Items.Add(lv);
+            }
+            connection.Close();
+        }
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //First row of all selected entries.
+            ListViewItem tempItem = listView1.SelectedItems[0];
+
+            this.ID = tempItem.SubItems[0].Text;
+            this.firstName = tempItem.SubItems[1].Text;
+            this.lastName = tempItem.SubItems[2].Text;
+
+            MessageBox.Show(tempItem.SubItems[1].Text);
+            //tempItem = listView1.SelectedItems[0];
+            //this.lastName = tempItem.Text;
+           // ListViewItem item = listView1.SelectedItems[2];
+           // this.firstName = tempItem.Text;
+
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           // this.get
+          //  setStudent("hello");
+            
+            this.Close();
+        }
+
+        private void SearchWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
